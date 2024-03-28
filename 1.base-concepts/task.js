546084319233306b6a1e08d7bedfad530fed1c51
Sergey Falov <srg.falov@yandex.ru>
2024-03-28 20:26:33 +0300

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,7 +21,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	percent = (percent * 0.01) / 12;
 	let paymentBody = amount - contribution;
 
-	let payment = paymentBody * (percent + (percent / (Math.pow(1 + percent, countMonths) - 1)));
+	let payment = paymentBody * (percent + (percent / (Math.pow(percent + 1, countMonths) - 1)));
 	
   for (let i = 0; i < countMonths; i++) {
 		paymentAmount = paymentAmount + payment;
@@ -30,3 +30,4 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
 	return paymentAmount;
 }
+
