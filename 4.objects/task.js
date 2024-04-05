f67f5c6f3ab150eea5dca5b1d51caf40b642a598
Sergey Falov <srg.falov@yandex.ru>
2024-04-05 20:09:56 +0300

    Задание готово!

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks === undefined) {
+		this.marks = [...marks];
+	} else {
+		this.marks.push(...marks);
+	}
 }
-
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	let sum = 0;
+	for (let i = 0; i < this.marks.length; i++) {
+		sum += this.marks[i];
+	}
+	return sum / this.marks.length;
 }
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
