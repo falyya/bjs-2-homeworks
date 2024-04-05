fff59e1db3fcaefe636ea518d205e2d22262d07a
Sergey Falov <srg.falov@yandex.ru>
2024-04-05 20:55:46 +0300

    Задание готово1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,30 +1,34 @@
-
 function Student(name, gender, age) {
-	this.name = name;
-	this.gender = gender;
-	this.age = age;
-}
-
-Student.prototype.setSubject = function(subjectName) {
-	this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function(...marks) {
-	if (this.marks === undefined) {
-		this.marks = [...marks];
-	} else {
-		this.marks.push(...marks);
-	}
-}
-Student.prototype.getAverage = function() {
-	let sum = 0;
-	for (let i = 0; i < this.marks.length; i++) {
-		sum += this.marks[i];
-	}
-	return sum / this.marks.length;
-}
-Student.prototype.exclude = function(reason) {
-	delete this.subject;
-	delete this.marks;
-	this.exclude = reason;
-}
+    this.name = name;
+     this.gender = gender;
+     this.age = age;
+     this.marks = [];
+   }
+   
+   Student.prototype.setSubject = function (subjectName) {
+     this.subject = subjectName;
+   }
+   
+   Student.prototype.addMarks = function (...marks) {
+     if (this.marks === undefined || this.marks.length === 0) {
+       this.marks = [...marks];
+     } else {
+       this.marks.push(...marks);
+     }
+   }
+   Student.prototype.getAverage = function () {
+      if (this.marks === undefined || this.marks.length === 0){
+        return 0;
+      } else {
+     let sum = 0;
+     for (let i = 0; i < this.marks.length; i++) {
+       sum += this.marks[i];
+     }
+     return sum / this.marks.length;
+      }
+   }
+   Student.prototype.exclude = function (reason) {
+     delete this.subject;
+     delete this.marks;
+     this.exclude = reason;
+   }
